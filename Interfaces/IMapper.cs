f5988a141973ef3dbb3907872c248a5233f75398
David Corey Duke <dcoreydukedev@gmail.com>
2025-03-19 18:37:01 -0500

    imcremental commit

---
+++ Interfaces/IMapper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DCoreyDuke.CodeBase.Interfaces
+{
+    /// <summary>
+    /// Defines a contract for mapping between different object types. 
+    /// Implementations will provide specific mapping logic.
+    /// </summary>
+    public interface IMapper
+    {
+    }
+
+    /// <summary>
+    /// Defines a mapping interface for converting between two types.
+    /// </summary>
+    /// <typeparam name="TSource">Represents the source type from which data will be mapped.</typeparam>
+    /// <typeparam name="TDestination">Represents the destination type to which data will be mapped.</typeparam>
+    public interface IMapper<TSource, TDestination> : IMapper
+    {
+        public static TDestination? Map()
+        {
+            return default;
+        }
+
+        public static TSource Unmapped { get; }
+        public static TDestination Mapped { get; }
+    }   
+}
--- obj/Debug/net8.0/DCoreyDuke.CodeBase.AssemblyInfo.cs
+++ obj/Debug/net8.0/DCoreyDuke.CodeBase.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCopyrightAttribute("2025, dcoreyduke")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Codebase created by dcoreyduke")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("2.0.1.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.0.01+736b8825f8d35765bb8ad646f56d878f556aa10b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.0.01+1ca2cac2ecd14fc3e60728305d98d99815ab11e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("DCoreyDuke.CodeBase")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DCoreyDuke.CodeBase")]
 [assembly: System.Reflection.AssemblyVersionAttribute("2.0.1.0")]
